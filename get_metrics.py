94147a712c01304cec0f0b1f43124546e89b0058
Houssem Chihoub <houssem.chihoub@databricks.com>
2023-10-18 12:55:55 +0200

    add variable

--- get_metrics.py
+++ get_metrics.py
@@ -9,6 +9,7 @@ import requests
 def medium_metrics():
     df: DataFrame = dlt.read("medium_clean")
     metricsDF = df.groupby("link").applyInPandas(get_metrics, schema="link string, claps double, readingTime double")
+    x =  ""
     # Join on original data, sort by number of claps descending
     finalDF = metricsDF.join(df, on = "link", how = "right_outer").sort(desc("claps"))
     return finalDF
